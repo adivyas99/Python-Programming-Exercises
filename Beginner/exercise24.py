80f4b8f185d54fa5b9e2fe71b64567a9e4d8905a
Aditya Vyas <adivyas99@gmail.com>
2020-10-31 08:13:23 +0530

    Update exercise24.py

--- Beginner/exercise24.py
+++ Beginner/exercise24.py
@@ -1,9 +1,13 @@
+## To get documantation of any keyword!
 print(abs.__doc__)
 print(int.__doc__)
 print(input.__doc__)
 
 def sq(x):
     '''This returns the square of number'''
+    x= float(x)
+    ## Press shift+tab in Safari & you'll get the above line in the documentation.
     return x*x
 
 print(sq.__doc__)
+##another method for this!
