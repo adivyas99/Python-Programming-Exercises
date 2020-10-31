d265925683bf2d42fec8b861613eebc2226998bb
Aditya Vyas <adivyas99@gmail.com>
2020-10-31 08:16:28 +0530

    Update exercise40.py

--- Beginner/exercise40.py
+++ Beginner/exercise40.py
@@ -1,3 +1,13 @@
+# Pythonic way
+#
 l=[1,2,3,4,5,6,7,8,9,10]
 e=filter(lambda x: x%2==0, l)
 print(list(e))
+
+##Another method-
+## Lay man method
+l_copy = []
+for i in l:
+  if i%2==0:
+    l_copy.append(i)
+print(l_copy,'\n')
